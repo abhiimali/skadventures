15aa0e7f720911221a8a66286024151a76bb11ff
Abhijit Mali <maliabhijit183@gmail.com>
2023-02-16 23:18:09 +0530

    added navbar and footer

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import StickyFooter from "./components/Footer/footer.jsx";
+import Header from "./components/Header/Header.jsx";
+const App = () => {
+  return (
+    <>
+       <Header/>
+
+
+
+      <StickyFooter/>
+    </>
+  );
+};
+
+export default App;
